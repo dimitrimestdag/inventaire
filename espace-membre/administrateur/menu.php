767137ce4e02fb408b94a5093109fad1bfd83313
Dimitri Mestdag <dmestdag@ankama.lan>
2018-12-21 14:04:14 +0100

    correction des bugs

--- espace-membre/administrateur/menu.php
+++ espace-membre/administrateur/menu.php
@@ -1,11 +1,15 @@
-<?php if ($_SESSION['id'] == 2) {
-echo '<table>';
-echo '	<tr>';
-echo '		<td width="70%" valign="top">';
-echo '			<br  />';
-echo 'Bonjour '.Membre::info($_SESSION['id'], 'nom').' '.Membre::info($_SESSION['id'], 'prenom').', '.ProtectEspace::compteJeton($_SESSION['id']);
-echo '			<br />';
-echo '		</td>';
-echo '	</tr>';
-echo '</table>';
-}?>
+<?php 
+if (isset($_SESSION)) {
+    if ($_SESSION['id'] == 2) {
+        echo '<table>';
+        echo '	<tr>';
+        echo '		<td width="70%" valign="top">';
+        echo '			<br  />';
+        echo 'Bonjour '.Membre::info($_SESSION['id'], 'nom').' '.Membre::info($_SESSION['id'], 'prenom').', '.ProtectEspace::compteJeton($_SESSION['id']);
+        echo '			<br />';
+        echo '		</td>';
+        echo '	</tr>';
+        echo '</table>';
+    }
+}
+?>
